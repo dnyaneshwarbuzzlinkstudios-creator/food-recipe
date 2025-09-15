04b22109e50a638a768e256209d414370a9b0caf
dnyaneshwarbuzzlinkstudios-creator <dnyaneshwar.buzzlinkstudios@gmail.com>
2025-09-15 18:30:00 +0530

    added fevicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cook with Judy & Flo - Food Recipes</title>
+  <link rel="icon" type="image/png" href="{{ asset('images/favicon.ico') }}">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
 @font-face {
